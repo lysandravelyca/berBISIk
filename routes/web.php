61ee75e6bcf294af466c98d5ac28256d12f3b99c
JesselynW <146332547+JesselynW@users.noreply.github.com>
2024-03-18 14:39:34 +0700

    bikin route ygy

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('beranda');
+});
+
+Route::get('/kamus', function(){
+    return view('kamus');
+});
+
+Route::get('/acara', function(){
+    return view('acara');
+});
+
+Route::get('/tentangKami', function(){
+    return view('tentangKami');
 });
