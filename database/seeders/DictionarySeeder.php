be87ef1d3fa272fd7be209bc546531b6a4eafec9
jsycw <jessyjcw@gmail.com>
2024-04-04 23:32:05 +0700

    db gambar

--- database/seeders/DictionarySeeder.php
+++ database/seeders/DictionarySeeder.php
@@ -21,7 +21,22 @@ class DictionarySeeder extends Seeder
         $words = [
             ['word' => 'halo', 'picture' => 'assets\dictionary\word\halo.png'],
             ['word' => 'selamat', 'picture' => 'assets\dictionary\word\selamat.png'],
-            ['word' => 'hari', 'picture' => 'assets\dictionary\word\hari.png']
+            ['word' => 'hari', 'picture' => 'assets\dictionary\word\hari.png'],
+            ['word' => 'semua', 'picture' => 'assets\dictionary\word\semua.png'],
+            ['word' => 'aku', 'picture' => 'assets\dictionary\word\aku.png'],
+            ['word' => 'temanteman', 'picture' => 'assets\dictionary\word\temanteman.png'],
+            ['word' => 'bahasaisyarat', 'picture' => 'assets\dictionary\word\bahasaisyarat.png'],
+            ['word' => 'orang', 'picture' => 'assets\dictionary\word\orang.png'],
+            ['word' => 'indonesia', 'picture' => 'assets\dictionary\word\indonesia.png'],
+            ['word' => 'untuk', 'picture' => 'assets\dictionary\word\untuk.png'],
+            ['word' => 'disabilitas', 'picture' => 'assets\dictionary\word\disabilitas.png'],
+            ['word' => 'tema', 'picture' => 'assets\dictionary\word\tema.png'],
+            ['word' => 'internasional', 'picture' => 'assets\dictionary\word\internasional.png'],
+            ['word' => 'tahun', 'picture' => 'assets\dictionary\word\tahun.png'],
+            ['word' => 'tuli', 'picture' => 'assets\dictionary\word\tuli.png'],
+            ['word' => 'mengucapkan', 'picture' => 'assets\dictionary\word\mengucapkan.png'],
+            ['word' => 'tepuktangan', 'picture' => 'assets\dictionary\word\tepuktangan.png'],
+            ['word' => 'perempuan', 'picture' => 'assets\dictionary\word\perempuan.png']
         ];
 
         foreach ($words as $word){
--- resources/views/detailAcara.blade.php
+++ resources/views/detailAcara.blade.php
@@ -9,4 +9,20 @@ extends('layout/layout')
 
 @section('content')
 {{-- insert code here --}}
-<div></div>
+<div class="info">
+    <div class="image">
+        <div class="img_kelas">
+            <img src="" alt="">
+        </div>
+               
+        <div class="desc_acara">
+            <p class="tipe_acara">Judul Kelas / Seminar / Webinar </p>
+            <p class="judul_kelas">Judul Kelas</p>
+            <p class="sesi_kelas">7/10 sesi</p>
+        </div>
+
+        <div class="persen">
+                {{-- ini progress barnya --}}
+        </div>
+    </div>
+</div>
