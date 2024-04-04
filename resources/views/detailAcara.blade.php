e4e12400ac12d88ee81bb3c381f9c728107ea2b3
jsycw <jessyjcw@gmail.com>
2024-04-04 21:35:15 +0700

    detail acara

---
+++ resources/views/detailAcara.blade.php
@@ -0,0 +1,12 @@
+extends('layout/layout')
+
+@section('title', 'Acara')
+
+<body>
+    <!-- css -->
+    <link rel="stylesheet" href="{{ asset('css/detailAcara.css') }}">
+</body>
+
+@section('content')
+{{-- insert code here --}}
+<div></div>
