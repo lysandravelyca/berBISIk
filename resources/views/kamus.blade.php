8e6e59f5c7b989859c63a825de1e156fca9b9ee9
ellenaitsme <a.ellena1898@gmail.com>
2024-04-07 22:16:45 +0700

    Fix CSS Dictionary and Showing Dictionary Items Only When There is a Search Query

--- resources/views/kamus.blade.php
+++ resources/views/kamus.blade.php
@@ -13,10 +13,6 @@
             <button type="submit" class="btnksg">HURUF</button>
             <label for="" class="btnfull">KATA DAN KALIMAT</label>
         </form>
-
-        
-        {{-- <button class="btnksg">KATA DAN KALIMAT</button> --}}
-        
     </div>
     
     <h2 class="judul">PENCARIAN KATA ATAU KALIMAT</h2>
@@ -44,8 +40,6 @@
                             </div>
                         @endforeach                      
                     </div>
-                {{-- {{ $kamusCari }} --}}
-                {{-- ini kayaknya pake ini --}}
                 @endif
             </div>     
     </div>
@@ -65,68 +59,4 @@
             </div>
         </div>
     </div>
-
-    {{-- <div>
-        <h2 class="katadsr">KATA DASAR BAHASA ISYARAT</h2>
-        <div class = "kata-dasar">
-            <div class="container1">
-                <div class="kotak">
-                    <img src="{{ asset('assets/halo.png') }}" alt="halo">
-                </div>
-                <div class="kotak">
-                    <img src="{{ asset('assets/hari.png') }}" alt="hari">
-                </div>
-                <div class="kotak">
-                    <img src="{{ asset('assets/aku.png') }}" alt="aku">
-                </div>
-                <div class="kotak">
-                    <img src="{{ asset('assets/teman-teman.png') }}" alt="teman-teman">
-                </div>
-                <div class="kotak">
-                    <img src="{{ asset('assets/tuli.png') }}" alt="tuli">
-                </div>
-                <div class="kotak">
-                    <img src="{{ asset('assets/selamat.png') }}" alt="selamat">
-                </div>
-                <div class="kotak">
-                    <img src="{{ asset('assets/ind.png') }}" alt="ind">
-                </div>
-                <div class="kotak">
-                    <img src="{{ asset('assets/bhsisyarat.png') }}" alt="bhsisyarat">
-                </div>
-                <div class="kotak">
-                <img src="{{ asset('assets/disabilitas.png') }}" alt="disabilitas">
-                </div>
-                <div class="kotak">
-                    <img src="{{ asset('assets/semua.png') }}" alt="semua">
-                </div>
-                <div class="kotak">
-                    <img src="{{ asset('assets/inter.png') }}" alt="inter">
-                </div>
-                <div class="kotak">
-                    <img src="{{ asset('assets/tepuk.png') }}" alt="tepuk">
-                </div>
-                <div class="kotak">
-                    <img src="{{ asset('assets/untuk.png') }}" alt="untuk">
-                </div>
-                <div class="kotak">
-                    <img src="{{ asset('assets/orang.png') }}" alt="orang">
-                </div>
-                <div class="kotak">
-                    <img src="{{ asset('assets/tema.png') }}" alt="tema">
-                </div>
-                <div class="kotak">
-                    <img src="{{ asset('assets/ucap.png') }}" alt="ucap">
-                </div>
-                <div class="kotak">
-                    <img src="{{ asset('assets/tahun.png') }}" alt="tahun">
-                </div>
-                <div class="kotak">
-                    <img src="{{ asset('assets/perempuan.png') }}" alt="perempuan">
-                </div>
-            </div>
-            
-        </div>
-    </div> --}}
-
 @endsection
