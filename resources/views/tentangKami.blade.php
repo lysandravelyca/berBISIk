f4d9480c73d602429bfe87c8d4f4aaf1305e1096
lilypetishere <lysandra.velyca02@gmail.com>
2024-04-26 16:20:37 +0700

    tentangkami

--- resources/views/tentangKami.blade.php
+++ resources/views/tentangKami.blade.php
@@ -8,16 +8,35 @@
 @section('content')
 {{-- insert code here --}}
 
-<div class="tinggi">
-    <!-- biar agak tinggian  -->
+<div>
+    <div class="tinggi">
+        <!-- biar agak tinggian  -->
+        <img src="{{ asset('assets/logoAja.png') }}" alt="photo logo">
+    </div>
+
+    <div class="header">
+        <span>Ber<span>BISI</span>k</span>
+        <p>BerbahasaIsyaratIndonesiaAsik</p>
+    </div>
+
+    <div class="cerita">
+
+        <div class="judul">
+        <span>CERITA BER<span>BISI</span>K</span>
+        </div>
+
+        <div class="para">
+        <p>Kami adalah tim yang berkomitmen untuk menyediakan aksesibilitas dan inklusi bagi 
+            semua orang dalam mempelajari bahasa isyarat. Dengan keyakinan bahwa setiap individu 
+            memiliki hak untuk berkomunikasi dengan bebas dan mandiri, kami berupaya untuk memberikan 
+            platform pembelajaran yang mudah diakses dan berorientasi pada pengguna. Dengan 
+            pengalaman yang mendalam dalam pendidikan dan teknologi, kami menggabungkan inovasi 
+            dengan pengetahuan mendalam tentang bahasa isyarat untuk menciptakan pengalaman 
+            pembelajaran yang menarik dan efektif.</p>
+        </div>
+    </div>
+    
 </div>
 
-<div class="header">
-
-    <img src="{{ asset('assets/logoAja.png') }}" alt="photo logo">
-    <br>x
-    <span>Ber<span>BISI</span>k</span>
-    <p>BerbahasaIsyaratIndonesiaAsik</p>
-</div>
 @endsection
 
