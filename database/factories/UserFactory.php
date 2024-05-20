1075d45cfe471491a5767ce903d11d33b49a7561
lilypetishere <lysandra.velyca02@gmail.com>
2024-05-20 09:32:40 +0700

    gnti pass user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,7 +25,7 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'phone' => '123',
             'email_verified_at' => now(),
-            'password' => Hash::make('$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'), // password
+            'password' => Hash::make('abc'), // password
             'remember_token' => Str::random(10),
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now()
