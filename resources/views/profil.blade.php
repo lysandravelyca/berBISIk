ea34a105fe1e659e3b5cbccf107c5911dc9009bf
Vallen Charissa <vallen.charissa@gmail.com>
2024-05-26 23:55:21 +0700

    benerin css profil stlh ditambah data history

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -7,26 +7,24 @@
 
 @section('content')
 
-    {{-- insert code here --}}
-
+    {{-- masukkan kode di sini --}}
 
     <div class="profcard">
         <div class="header">
             Profil Saya
         </div>
         <div class="card">
-                @if (Auth::user()->photo != null)
-                    <img src="{{ asset('assets/fotoUser/' . Auth::user()->photo) }}" alt="">
-                @else
-                    {{-- kasih image sample --}}
-                    <img src="" alt="">
-                @endif
-            <div class = "buttons">
+            @if (Auth::user()->photo != null)
+                <img src="{{ asset('assets/fotoUser/' . Auth::user()->photo) }}" alt="">
+            @else
+                {{-- kasih image sample --}}
+                <img src="" alt="">
+            @endif
+            <div class="buttons">
                 <div class="btn">Ubah Cover</div>
                 <div class="btn2">Ubah Profil</div>
             </div>  
         </div>
-
     </div>
 
     <div class="history">
@@ -53,13 +51,11 @@
                                 @if (Auth::user()->role_id == 2)
                                     <div class="modify_button_container">
                                         <div class="modify_button">
-                                            <a href="ubahAcara/{{ $acaraUser->events->id }}"><img src="assets/icon/edit.png"
-                                                    alt=""></a>
+                                            <a href="ubahAcara/{{ $acaraUser->events->id }}"><img src="assets/icon/edit.png" alt=""></a>
                                         </div>
 
                                         <div class="modify_button">
-                                            <a href="hapusAcara/{{ $acaraUser->events->id }}"><img
-                                                    src="assets/icon/trash.png" alt=""></a>
+                                            <a href="hapusAcara/{{ $acaraUser->events->id }}"><img src="assets/icon/trash.png" alt=""></a>
                                         </div>
                                     </div>
                                 @endif
@@ -72,8 +68,7 @@
                                     <h3 class="judul_acara">{{ $acaraUser->events->title }}</h3>
                                     <div class="pengajar">
                                         <div class="profil_pengajar">
-                                            <img src="{{ asset('assets/fotoPengajar/' . $acaraUser->events->instructors->photo) }}"
-                                                alt="">
+                                            <img src="{{ asset('assets/fotoPengajar/' . $acaraUser->events->instructors->photo) }}" alt="">
                                         </div>
 
                                         <div class="deskripsi_pengajar">
@@ -98,7 +93,8 @@
                                         <h3>{{ substr($acaraUser->events->event_schedules->first()->time_start, 0, -3) }}
                                             -
                                             {{ substr($acaraUser->events->event_schedules->first()->time_end, 0, -3) }}
-                                            WIB</h3>
+                                            WIB
+                                        </h3>
                                     </div>
 
                                     <div class="rating">
@@ -111,8 +107,9 @@
                                     </div>
 
                                     <h3>Sudah terdaftar</h3>
-
+                                </a>
                             </div>
+                        </div>
                     </div>
                 </a>
             @endforeach
@@ -126,58 +123,56 @@
                 <h2>Kelas yang Sedang Berlangsung</h2>
             </div>
         </div>
-            <div class="container">
-                @foreach($daftarAcaraRelawanUser as $acaraRelawanUser)
+        <div class="container">
+            @foreach($daftarAcaraRelawanUser as $acaraRelawanUser)
                 <a href="volunteer/{{ $acaraRelawanUser->volunteer_events->id }}">
-                <div class="c2_relawan">
-                    <div class="kelas_card">
-                        <div class="img_container">
+                    <div class="c2_relawan">
+                        <div class="kelas_card">
+                            <div class="img_container">
+                                <div class="img_kelas">
+                                    <img src="{{ asset('assets/fotoVolunteer/'.$acaraRelawanUser->volunteer_events->photo) }}" alt="">
+                                </div>
                                 
-                            <div class="img_kelas">
-                                <img src="{{ asset('assets/fotoVolunteer/'.$acaraRelawanUser->volunteer_events->photo)  }}" alt="">
+                                @if(Auth::user()->role_id == 2)
+                                    <div class="modify_button_container">
+                                        <div class="modify_button">
+                                            <a href="ubahVolunteer/{{ $acaraRelawanUser->volunteer_events->id }}"><img src="assets/icon/edit.png" alt=""></a>
+                                        </div>
+                        
+                                        <div class="modify_button">
+                                            <a href="hapusVolunteer/{{ $acaraRelawanUser->volunteer_events->id }}"><img src="assets/icon/trash.png" alt=""></a>
+                                        </div>
+                                    </div>
+                                @endif
+                                
                             </div>
-                            
-                            @if(Auth::user()->role_id == 2)
-                            <div class="modify_button_container">
-                                <div class="modify_button">
-                                    <a href="ubahVolunteer/{{ $acaraRelawanUser->volunteer_events->id }}"><img src="assets/icon/edit.png" alt=""></a>
-                                </div>
-                
-                                <div class="modify_button">
-                                    <a href="hapusVolunteer/{{ $acaraRelawanUser->volunteer_events->id }}"><img src="assets/icon/trash.png" alt=""></a>
-                                </div>
-                            </div>
-                            @endif
-                            
-                        </div>
-        
-                        <div class="cardKelas">
-                            <a href="volunteer/{{ $acaraRelawanUser->volunteer_events->id }}">
-                            <h3>{{ $acaraRelawanUser->volunteer_events->title }}</h3>
-                
-                            <div class="icon_relawan">
-                                <img src="assets/icon/location.png" alt="">
-                                <h3>{{ $acaraRelawanUser->volunteer_events->location}}</h3>
-                            </div>
-                
-                            <div class="icon_relawan">
-                                <img src="assets/icon/calendar.png" alt="">
-                                <h3>{{ $acaraRelawanUser->volunteer_events->volunteer_event_schedules->date->format('j M Y') }}</h3>
-                            </div>
-                
-                            <div class="icon_relawan">
-                                <img src="assets/icon/clocktime.png" alt="">
-                                <h3>{{ substr($acaraRelawanUser->volunteer_events->volunteer_event_schedules->time_start, 0, -3) }} - 
-                                    {{ substr($acaraRelawanUser->volunteer_events->volunteer_event_schedules->time_end, 0, -3)}} WIB</h3>
+            
+                            <div class="cardKelas">
+                                <a href="volunteer/{{ $acaraRelawanUser->volunteer_events->id }}">
+                                    <h3>{{ $acaraRelawanUser->volunteer_events->title }}</h3>
+                        
+                                    <div class="icon_relawan">
+                                        <img src="assets/icon/location.png" alt="">
+                                        <h3>{{ $acaraRelawanUser->volunteer_events->location}}</h3>
+                                    </div>
+                        
+                                    <div class="icon_relawan">
+                                        <img src="assets/icon/calendar.png" alt="">
+                                        <h3>{{ $acaraRelawanUser->volunteer_events->volunteer_event_schedules->date->format('j M Y') }}</h3>
+                                    </div>
+                        
+                                    <div class="icon_relawan">
+                                        <img src="assets/icon/clocktime.png" alt="">
+                                        <h3>{{ substr($acaraRelawanUser->volunteer_events->volunteer_event_schedules->time_start, 0, -3) }} - 
+                                            {{ substr($acaraRelawanUser->volunteer_events->volunteer_event_schedules->time_end, 0, -3)}} WIB
+                                        </h3>
+                                    </div>
+                                </a>
                             </div>
                         </div>
                     </div>
-                </div>
-                
                 </a>
-        
-                @endforeach
-            </div>
+            @endforeach
         </div>
     </div>
 @endsection
