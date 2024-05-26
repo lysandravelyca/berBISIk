819800a1d833968e267f4278f2560570c3a92cf0
jsycw <jessyjcw@gmail.com>
2024-05-26 21:47:59 +0700

    css

--- resources/views/detailVolunteer.blade.php
+++ resources/views/detailVolunteer.blade.php
@@ -1,11 +1,13 @@
 @extends('layout/layout')
 
+<link rel="shortcut icon" type="image/png" href="{{ asset('assets/logoAja.png') }}">
+
 @section('title', $volunteer->title)
 
 <link rel="stylesheet" href="{{ asset('css/detailVolunteer.css') }}">
 <script src="https://kit.fontawesome.com/c1fc3d2826.js" crossorigin="anonymous"></script>
-@section('content')
 
+@section('content')
 <div class="back">
     <a href="/acara">< Kembali</a>
 </div>
@@ -92,3 +94,4 @@
         </div>
     </div>
 </div>
+@endsection
