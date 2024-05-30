2490f5481e5790a162e2f11ce6b400a6f2588815
ellena <86466041+ellenaitsme@users.noreply.github.com>
2024-05-30 11:42:26 +0700

    Volunteer Event Register + EventScheduleController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Models\UsersEvent;
 use App\Models\UsersVolunteerEvent;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -12,10 +13,15 @@ class UserController extends Controller
 {
     public function show()
     {
+        $currentDate = Carbon::now();
         $userId = Auth::user()->id;
 
         $userEvent = UsersEvent::with('events', 'events.event_types', 'events.instructors', 'events.event_schedules')
-                                ->where('user_id', $userId)->get();
+            ->where('user_id', $userId)
+            // ->whereHas('events.event_schedules', function ($query) use ($currentDate) {
+            //     $query->where('date', '>', $currentDate);
+            // })
+        ->get();
 
         $userVolunteerEvent = UsersVolunteerEvent::with('volunteer_events', 'volunteer_events.volunteer_event_schedules')
                                 ->where('user_id', $userId)->get();
