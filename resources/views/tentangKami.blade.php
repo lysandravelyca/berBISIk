1fdc0cd891ea656460876ba5ddea9a0b4462220f
ellena <86466041+ellenaitsme@users.noreply.github.com>
2024-06-04 17:14:08 +0700

    Tentang Kami Responsive Done

--- resources/views/tentangKami.blade.php
+++ resources/views/tentangKami.blade.php
@@ -9,28 +9,40 @@
 {{-- insert code here --}}
 
 <div>
-    <div class="header">
-        <img src="{{ asset('assets/logoOnly.png') }}" alt="photo logo">
-        <span>Ber<span>BISI</span>k</span>
-        <p>#BerbahasaIsyaratIndonesiaAsik</p>
+    <div class="header-container">
+        <div class="header-bg">
+            <img class="bg" src="{{ asset('assets/tentangKami/backgroundLeft.png') }}" alt="">
+            <img class="bg-tab" src="{{ asset('assets/tentangKami/backgroundRight.png') }}" alt="">
+        </div>
+        
+        <div class="header">
+            <div class="title">
+                <img src="{{ asset('assets/logoOnly.png') }}" alt="photo logo">
+                <h1 >Ber<span>BISI</span>k</h1>
+            </div>
+            
+            <p>#BerbahasaIsyaratIndonesiaAsik</p>
+        </div>
+        <img class="bg" src="{{ asset('assets/tentangKami/backgroundRight.png') }}" alt="">
     </div>
+   
 
-    <section>
+    <section class="cerita-section">
         <div class="cerita">
 
-            <div class="judul">
-                <span>CERITA BER<span>BISI</span>K</span>
+            {{-- <div class="judul"> --}}
+                <h2>CERITA BER<span>BISI</span>K</h2>
+            {{-- </div> --}}
+            
+            <div class="content">
+                
+                <img id="circle-left" src="{{ asset('assets/tentangKami/circleLeft.png') }}" alt="">
+                <div class="para">
+                    <p>Kami adalah tim yang ber<span>komitmen</span> untuk menyediakan <span>aksesibilitas</span> dan <span>inklusi</span> dalam mempelajari bahasa isyarat. Kami percaya setiap individu berhak berkomunikasi secara <span>bebas</span> dan <span>mandiri.</span> Dengan pengalaman dalam pendidikan dan teknologi, kami menciptakan platform pembelajaran bahasa isyarat yang <span class="gradient-text">mudah diakses, inovatif, dan efektif.</span></p>
+                </div>
+                <img id="circle-right" src="{{ asset('assets/tentangKami/circleRight.png') }}" alt="">
             </div>
-    
-            <div class="para">
-            <p>Kami adalah tim yang berkomitmen untuk menyediakan aksesibilitas dan inklusi bagi 
-                semua orang dalam mempelajari bahasa isyarat. Dengan keyakinan bahwa setiap individu 
-                memiliki hak untuk berkomunikasi dengan bebas dan mandiri, kami berupaya untuk memberikan 
-                platform pembelajaran yang mudah diakses dan berorientasi pada pengguna. Dengan 
-                pengalaman yang mendalam dalam pendidikan dan teknologi, kami menggabungkan inovasi 
-                dengan pengetahuan mendalam tentang bahasa isyarat untuk menciptakan pengalaman 
-                pembelajaran yang menarik dan efektif.</p>
-            </div>
+            
         </div>
     </section>
     
@@ -47,10 +59,10 @@
                 <span>VISI KAMI</span>
             </div>
         </div>
-    </section>
+    
     
 
-    <section class="misi-section">
+    
         <div class="misi">
 
             <div class="misiKami">
@@ -86,7 +98,7 @@
                 <img src="{{ asset('assets/el.png') }}" alt="">
             </div>
             <div class="nama">
-                <h1>DIREKTUR UTAMA</h1>
+                <h2>DIREKTUR UTAMA</h1>
                 <P>Anastashia Ellena Widjaja</P>
             </div>
     </div>
@@ -97,7 +109,7 @@
                 <img src="{{ asset('assets/jeje.png') }}" alt="">
             </div>
             <div class="nama">
-                <h1>KEPALA KURIKULUM</h1>
+                <h2>KEPALA KURIKULUM</h1>
                 <P>Jesselyn Widjaja</P>
             </div>
     </div>
@@ -109,7 +121,7 @@
                 <img src="{{ asset('assets/jessy.png') }}" alt="">
             </div>
             <div class="nama">
-                <h1>KEPALA PEMASARAN</h1>
+                <h2>KEPALA PEMASARAN</h1>
                 <P>Jessy Clarissa Wijaya</P>
             </div>
     </div>
@@ -121,7 +133,7 @@
                 <img src="{{ asset('assets/ly.png') }}" alt="">
             </div>
             <div class="nama">
-                <h1>PENGEMBANG WEB</h1>
+                <h2>PENGEMBANG WEB</h1>
                 <P>Lysandra Velyca</P>
             </div>
         </div>
@@ -133,105 +145,120 @@
                 <img src="{{ asset('assets/val.png') }}" alt="">
             </div>
             <div class="nama">
-                <h1>PERANCANG GRAFIS</h1>
+                <h2>PERANCANG GRAFIS</h1>
                 <p>Vallen Charissa Gunawan</p>
             </div>
         </div>
     </div>
     
     </div>
+    <div class="advertisement-container">
+        <div class="advertisement-box">
+            <img src="assets/tentangKami/peserta.png">
+            <h2>378</h2>
+            <p>Peserta Program Kami</p>
+        </div>
+
+        <div class="advertisement-box">
+            <img src="assets/tentangKami/provinsi.png">
+            <h2>25</h2>
+            <p>Provinsi di Indonesia</p>
+        </div>
+        <div class="advertisement-box">
+            <img src="assets/tentangKami/paket.png">
+            <h2>32</h2>
+            <p>Paket Belajar</p>
+        </div>
+    </div>  
 </section>
-    
-    
-
-        <div class="semua">
-            <div class="peserta">
-                <img src="assets/peserta.png">
-                <h1>378</h1>
-                <p>Peserta Program Kami</p>
-            </div>
-
-            <div class="provinsi">
-                <img src="assets/provinsi.png">
-                <h1>25</h1>
-                <p>Provinsi di Indonesia</p>
-            </div>
-            <div class="paket">
-                <img src="assets/paket.png">
-                <h1>32</h1>
-                <p>Paket Belajar</p>
-            </div>
-        </div>  
         
-        <div class="bisik">
-            <div class="bisi">
-
-                <h1><span>BISI</span>KAN MEREKA </h1>
-
-                <div class="satu">
-
-                    <div class="jeno">
-
-                        <div class="all" >
-
-                            <div class="nama">
-
-                                <h2>JENO</h2>
-                                <p>Mahasiswa</p>
-                            </div>
-
-                                <div class="rating">
-                                    <img src="assets/tang.png" alt="">
-                                    <img src="assets/tang.png" alt="">
-                                    <img src="assets/tang.png" alt="">
-                                    <img src="assets/tang.png" alt="">
-                                    <img src="assets/tang.png" alt="">
+        
+        <section class="bisikan-mereka-section">
+            <div class="bisik">
+                <div class="bisi">
+    
+                    <h2><span>BISI</span>KAN MEREKA </h2>
+    
+                    <div class="comment-container">
+                        <div class="comment-box">
+                            <div class="comment">
+                                <img class="profile-picture" src="{{ asset('assets/tentangKami/jeno.png') }}" alt="">
+                                <div class="content">
+                                    <div class="name-rating">
+                                        <div class="name-desc">
+                                            <h3>JENO</h3>
+                                            <p>Mahasiswa</p>
+                                        </div>
+                                        <img src="{{ asset('assets/tentangKami/rating.png') }}" alt="">
+                                    </div>
+                                    <p>Saya sering lihat penerjemah bahasa isyarat di TV
+                                        dan saya tertarik untuk belajar tapi sulit untuk dipahami sendiri.
+                                        Setelah mengikuti pembelajaran di berBISIk, materi terasa lebih
+                                        mudah karena dibimbing sampai bisa. Pengajarnya juga
+                                        memastikan sampai saya paham.</p>
                                 </div>
+                            </div>
+                            <img class="triangle" src="{{ asset('assets/tentangKami/triangle.png') }}" alt="">
                         </div>
                         
-
-                        <div class=text>
-                            <p>Saya sering lihat penerjemah bahasa isyarat di TV
-                                dan saya tertarik untuk belajar tapi sulit untuk dipahami sendiri.
-                                Setelah mengikuti pembelajaran di berBISIk, materi terasa lebih
-                                mudah karena dibimbing sampai bisa. Pengajarnya juga
-                                memastikan sampai saya paham</p>
+                        <div class="comment-box">
+                            <div class="comment">
+                                <img class="profile-picture" src="{{ asset('assets/tentangKami/melisa.png') }}" alt="">
+                                <div class="content">
+                                    <div class="name-rating">
+                                        <div class="name-desc">
+                                            <h3>MELISA</h3>
+                                            <p>Pelajar</p>
+                                        </div>
+                                        <img src="{{ asset('assets/tentangKami/rating.png') }}" alt="">
+                                    </div>
+                                    <p>Sebagai seseorang yang baru memulai belajar bahasa isyarat, saya sangat terkesan dengan pengalaman menggunakan website pembelajaran ini. Materi yang disajikan sangat mudah dipahami dan interaktif. Saya merasa semakin percaya diri dalam berkomunikasi dengan penyandang tunarungu.</p>
+                                </div>
+                            </div>
+                            <img class="triangle" src="{{ asset('assets/tentangKami/triangleLeft.png') }}" alt="">
                         </div>
-        
+    
+                        <div class="comment-box">
+                            <div class="comment">
+                                <img class="profile-picture" src="{{ asset('assets/tentangKami/namju.png') }}" alt="">
+                                <div class="content">
+                                    <div class="name-rating">
+                                        <div class="name-desc">
+                                            <h3>NAMJU</h3>
+                                            <p>Pekerja</p>
+                                        </div>
+                                        <img src="{{ asset('assets/tentangKami/rating.png') }}" alt="">
+                                    </div>
+                                    <p>Saya sangat senang dengan website pembelajaran bahasa isyarat ini! Materinya mudah dipahami dan membantu saya belajar dengan cepat. Terima kasih!</p>
+                                </div>
+                            </div>
+                            <img class="triangle" src="{{ asset('assets/tentangKami/triangle.png') }}" alt="">
+                        </div>
+    
+                        <div class="comment-box">
+                            <div class="comment">
+                                <img class="profile-picture" src="{{ asset('assets/tentangKami/rachel.png') }}" alt="">
+                                <div class="content">
+                                    <div class="name-rating">
+                                        <div class="name-desc">
+                                            <h3>RACHEL</h3>
+                                            <p>Ibu Rumah Tangga</p>
+                                        </div>
+                                        <img src="{{ asset('assets/tentangKami/rating.png') }}" alt="">
+                                    </div>
+                                    <p>Selain mendapatkan materi, saya juga mendapat kesempatan untuk mengikuti kegiatan volunteer melalui platform ini. Pengalaman ini sangat berharga bagi saya karena saya bisa langsung mengaplikasikan apa yang telah dipelajari dalam situasi nyata. </p>
+                                </div>
+                            </div>
+                            <img class="triangle" src="{{ asset('assets/tentangKami/triangleLeft.png') }}" alt="">
+                        </div>
+    
+                    </div>
+    
                         
-                    </div>  
-
-                    <div class="Melisa">
-                        <h2>MELISA</h2>
-                        <p>Pelajar</p>
-                        <p>Sebagai seseorang yang baru memulai belajar bahasa isyarat, saya sangat
-                            terkesan dengan pengalaman menggunakan website pembelajaran ini. Materi 
-                            yang disajikan sangat mudah dipahami dan interaktif. Saya merasa semakin 
-                            percaya diri dalam berkomunikasi dengan penyandang tunarungu.</p>
-                    </div>
-
-                    <div class="Mark">
-                        <h2>MARK</h2>
-                        <p>Pekerja</p>
-                        <p>Saya sering lihat penerjemah bahasa isyarat di TV
-                            dan saya tertarik untuk belajar tapi sulit untuk dipahami sendiri.
-                            Setelah mengikuti pembelajaran di berBISIk, materi terasa lebih
-                            mudah karena dibimbing sampai bisa. Pengajarnya juga
-                            memastikan sampai saya paham</p>
-                    </div>
-
-                    <div class="Rachel">
-                        <h2>RACHEL</h2>
-                        <p>Ibu Rumah Tangga</p>
-                        <p>Selain mendapatkan materi, saya juga mendapat kesempatan untuk mengikuti 
-                            kegiatan volunteer melalui platform ini. Pengalaman ini sangat berharga bagi 
-                            saya karena saya bisa langsung mengaplikasikan apa yang telah dipelajari dalam 
-                            situasi nyata. </p>
-                    </div>
-
                 </div>
             </div>
-        </div>
+        </section>
+        
 
         <div class="faq">
 
@@ -330,10 +357,12 @@
             
             if(icon.classList.contains('active')) {
                 icon.classList.remove('active');
+                answer.classList.remove('active');
                 answer.style.maxHeight = null;
             } else {
                 icon.classList.add('active');
-                answer.style.maxHeight = answer.scrollHeight + 'px';
+                answer.style.maxHeight = answer.scrollHeight + 8 + 'px';
+                answer.classList.add('active');
             }
             
         })
